4b014983d9ee94c0d3b2e1de7f6f6f9f0ac37869
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-10-24 15:07:20 -0400

    finished Lab

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -11,31 +11,68 @@ public class Board {
     }
 
     public boolean checkHorizontal(Character xO){
+        for (int i = 0; i <= 2; i++){
+            if (board[i][0] == xO && board[i][1] == xO && board[i][2] == xO){
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean checkVertical(Character xO){
-        return false;
-    }
+    public boolean checkVertical(Character xO) {
+        for (int i = 0; i <= 2; i++) {
+            if (board[0][i] == xO && board[1][i] == xO && board[2][i] == xO) {
+                return true;
+            }
+        }
+            return false;
+        }
+
 
     public boolean checkDiagonal(Character xO){
-        return false;
-    }
+            if ((board[0][0] == xO && board[1][1] == xO && board[2][2] == xO) || (board[0][2] == xO && board[1][1] == xO && board[2][0] == xO)) {
+                return true;
+            } else {
+
+                return false;
+            }
+        }
 
     public Boolean isInFavorOfX() {
-        return false;
+        if (checkHorizontal('X') || checkVertical('X') || checkDiagonal('X')) {
+            return true;
+
+    }else{
+            return false;
+        }
+
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if (checkHorizontal('O') || checkVertical('O') || checkDiagonal('O')) {
+            return true;
+
+        }else{
+            return false;
+        }
+
     }
-        
+
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfX() && isInFavorOfO()) {
+            return true;
+        }
+        return false;
     }
-
     public String getWinner() {
-        return null;
-    }
+        if(isInFavorOfO()){
+            return "O";
+            }
+        if(isInFavorOfX()){
+            return "X";
 
+            }
+            return "";
+    }
 }
+
